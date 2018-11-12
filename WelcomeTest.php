45452c34458b2864207ffb44019282f7591a3137
Olivier D <odonfack@donaph01.Speedport_W_724V_Typ_A_05011603_06_001>
2018-11-12 23:53:43 +0100

    add welcome and welcometest

---
+++ WelcomeTest.php
@@ -0,0 +1,17 @@
+<?php
+	use PHPUnit\Framework\TestCase;
+	require 'welcome.php';
+	class WelcomeTest extends TestCase {
+	  protected function setUp (){
+	    $this ->welcome = new welcome();
+	  }
+	
+	  protected function tearDown() {
+	    $this->welcome = NULL;
+	  }
+
+	  public function testGreet() {
+	    $result = $this->welcome->greet();
+	    $this->assertEquals("Welcome to DevOps on AWS",$result);
+	  }
+	}
